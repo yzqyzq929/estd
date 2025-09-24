73ccf1d9912dccb86ae02019a7e6b47965e6c76d
yzqyzq929 <3966219411@qq.com>
2025-09-24 14:35:53 +0800

    Add files via upload
    Some assert function.

--- estd_assert.cpp
+++ estd_assert.cpp
@@ -3,37 +3,55 @@
 #include"estd_assert.h"
 
 namespace estd {
+
 	void dynamic(bool __assert,
 		const char* __message,
 		short __assert_level,
 		error_level __level,
 		const char* __file,
 		const char* __function,
-		int __line) {
+		int __line,
+		assert_handle __handle) {
 
-		if (__assert||__assert_level>assert_level)
+		if (__assert || __assert_level > assert_level)
 			return;
 
-		std::ostringstream oss;
-		oss << "Debug assertion failed!\n\n";
-		if(__file != "")
-			oss << "File:"
-				<< __file;
-		if(__function != "")
-			oss << "\nFunction:"
-			    << __function;
-
-		if (!__line) { 
-			oss << "\nLine:"
-				<< __line;
-		}
-
-		oss <<"\n\nExpression:"
-			<< __message
-			<<" \n";
-
-		std::cerr << oss.str();
+		if(__handle == _print||__handle == _print_abort||__handle == _print_throw)
+			__print_error(std::cout, __message, __level, __file, __function, __line);
+		if (__handle == _abort || __handle == _print_abort)
+			std::terminate();
+		if (__handle == _throw || __handle == _print_throw)
+			throw __exception_maker(
+				__message,
+				__level,
+				__file,
+				__function,
+				__line
+			);
+		
 	}
+	
+	void dynamic(bool __assert,
+		short __assert_level,
+		assert_error __error,
+		assert_handle __handle = default_assert_handle) {
+		return dynamic(__assert,
+			__error.message, 
+			__assert_level, 
+			__error.level, 
+			__error.file, 
+			__error.function, 
+			__error.line,
+			__handle);
+	}
+
+	short assert_level = __estd_default_assert_level;
+	const std::map<error_level, std::string> error_level_message = {
+		{error_level::error,"Error"},
+		{error_level::warning,"Warning"},
+		{error_level::information,"Information"},
+		{error_level::log,"Log"}
+	};
 }
 
 #endif
--- estd_assert.h
+++ estd_assert.h
@@ -2,39 +2,66 @@
 #include<string>
 #include<sstream>
 #include<map>
-#include<vector>
+#include<exception>
 
 namespace estd {
 
+	/*
+	* enum assert_handle
+	* Handle when assert failed.
+	* It has 5 member:
+	* _abort       Call std::terminate().
+	* _throw       Throw assert_error.
+	* _print       Print error.
+	* _print_abort Print error, and abort.
+	* _print_throw Print error, and throw error.
+	*/
+	enum assert_handle : char {
+		_abort,
+		_throw,
+		_print,
+		_print_abort,
+		_print_throw
+	};
+
+	/*
+	* assert_handle default_assert_handle
+	* Default handle when assert failed.
+	* The value is assert_handle::_print.
+	*/
+	constexpr assert_handle default_assert_handle = assert_handle::_print;
+	
 	/*
 	* short __estd_default_assert_level
 	* Default assert level.
 	* The value is 5.
 	*/
-	constexpr short __estd_default_assert_level = 5;  
+	constexpr unsigned short __estd_default_assert_level = 5;  
 
 	/*
-	* int assert_level
+	* extern int assert_level
 	* User can set this assert level, so it's mutable.
 	* It has 5 level:
-	* 1     Weak check
-	* 2     Release check
-	* 3     Normal check
-	* 4     Debug check
+	* 1     Weak check.
+	* 2     Release check.
+	* 3     Normal check.
+	* 4     Debug check.
 	* 5     Strong check.
+	* 
+	* It will define in .cpp file.
 	*/
-	short assert_level = __estd_default_assert_level;
+	extern short assert_level;
 
 	/*
 	* enum error_level
 	* Level of error.
 	* 4 levels:
-	* error
-	* warning
-	* information
-	* log
+	* error.
+	* warning.
+	* information.
+	* log.
 	*/
-	enum error_level : int {
+	enum error_level : char {
 		error,
 		warning,
 		information,
@@ -42,16 +69,114 @@ namespace estd {
 	};
 
 	/*
-	* std::map error_levevl_message
+	* extern std::map error_levevl_message
 	* Get error level's string.
+	* It will define in .cpp file.
 	*/
-	const std::map<error_level, std::string> error_level_message = {
-		{error_level::error,"Error"},
-		{error_level::warning,"Warning"},
-		{error_level::information,"Infomation"},
-		{error_level::log,"Log"}
-	};
+	extern const std::map<error_level, std::string> error_level_message;
 	
+	/*
+	* class assert_error
+	* exception based from assert_error
+	* For dynamic assert.
+	*/
+	class assert_error : public std::exception {
+	public:
+		using base = std::exception;
+		using error_type = const char*;
+
+		explicit assert_error(error_type _e) :message{ _e }
+		{
+		}
+
+		assert_error(const char* _m,
+			error_level _l,
+			const char* _file = "",
+			const char* _func = "",
+			int _line = -1) : message{ _m },
+			level{ _l },
+			file{ _file },
+			function{ _func },
+			line{ _line } {
+
+		}
+		
+		inline const char* what() const override {
+			return message;
+		}
+
+		//Assert information.
+		const char* message;
+		error_level level{ error_level::error };
+		const char* file{};
+		const char* function{};
+		int line{ -1 };  //Default line.
+	};
+
+	/*
+	* function __exception_maker
+	* Helper function for dynamic() and user.
+	*/
+	inline assert_error __exception_maker(
+		const char* __message,
+		error_level __level,
+		const char* __file = "",
+		const char* __function = "",
+		int __line = -1) {
+		return assert_error{
+			__message,__level,__file,__function,__line
+		};
+	}
+	/*
+	* template<Om> function __print_error
+	* Helper function for dynamic().
+	*/
+	template<typename _Om = std::ostream>
+	_Om& __print_error(_Om& __out_stream,
+		const char* __message,
+		error_level __level,
+		const char* __file = "",
+		const char* __function = "",
+		int __line = -1) {
+		std::ostringstream oss;
+		oss << "Debug assertion failed!\n\n";
+		if (__file != "")
+			oss << "File:"
+			<< __file;
+		if (__function != "")
+			oss << "\nFunction:"
+			<< __function;
+
+		if (__line != -1) {
+			oss << "\nLine:"
+				<< __line;
+		}
+
+		oss << "\n\nExpression:"
+			<< __message
+			<< " \n";
+
+		return __out_stream << oss.str();
+	}
+
+	/*
+	* Another template<_Om> function __print_error
+	* Print out assert_error.
+	*/
+	template<typename _Om = std::ostream>
+	inline _Om& __print_error(_Om& __out_stream,
+		assert_error __error
+	) {
+		return __print_error<_Om>(
+			__out_stream,
+			__error.message,
+			__error.level,
+			__error.file,
+			__error.function,
+			__error.line
+		);
+	}
+
 	/*
 	* function dynamic
 	* bool           __assert			Assert.
@@ -85,7 +210,19 @@ namespace estd {
 		error_level __level,
 		const char* __file = "",
 		const char* __function = "",
-		int __line = -1);
+		int __line = -1,
+		assert_handle __handle = default_assert_handle);
+	/* 
+	* Another function dynamic
+	* Args:
+	* bool             __assert			No need say more..
+	* short            __assert_level	No need say more..
+	* assert_error     __error			Include error infomations.
+	*/
+	void dynamic(bool __assert,
+		short __assert_level,
+		assert_error __error,
+		assert_handle __handle = default_assert_handle);
 }
 
 #include"estd_assert.cpp"
